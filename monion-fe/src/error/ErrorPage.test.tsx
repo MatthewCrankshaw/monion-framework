78200a5fb239fcc21a55b5bebcf07d14fbf2df80
MatthewCrankshaw <matthew-crankshaw@idexx.com>
2024-03-30 19:45:30 +1300

    Add fe unit test workflow.

---
+++ monion-fe/src/error/ErrorPage.test.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { ErrorPage } from "./ErrorPage";
+
+describe("ErrorPage", () => {
+  it("renders the error message", () => {
+    render(<ErrorPage />);
+    const errorMessage = screen.getByText("Oops! Something went wrong");
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it("renders the menu icon", () => {
+    render(<ErrorPage />);
+    const menuIcon = screen.getByLabelText("menu");
+    expect(menuIcon).toBeInTheDocument();
+  });
+});
